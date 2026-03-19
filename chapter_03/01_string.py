7b1803b9adb9cb9f8455fbcbc7031dcbd1f11a00
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-20 01:26:13 +0530

    String manupulation

--- chapter_03/01_string.py
+++ chapter_03/01_string.py
@@ -1,2 +1,9 @@
 
 #! Chapter 03 Strings
+
+
+name =  "Adarsh Bhardwaj"
+
+nameShort = name[:7] #? name[initial_index:excluding_index]
+
+print(nameShort)
---
+++ chapter_03/02_negative_slicing.py
@@ -0,0 +1,22 @@
+
+#! Negative Slicing
+
+
+name =  "Adarsh Bhardwaj"
+
+
+print(name[0:7])
+
+print(name[-8:-1]+" <--->")
+print(name[1:8])
+
+
+
+print(name[:7]) #? name[initial_index:excluding_index] 
+#! name[default_is_0:default_is_len_of_string]
+
+print(len(name))
+print(name[0:15:2]+" --- ")
+
+
+#!  01:30:00 
