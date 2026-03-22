f337a01d2fe5aadba37d706682210e89650b9a7a
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-23 00:27:42 +0530

    learning some string functions and escape sequence in python

---
+++ chapter_03/03_str_functions.py
@@ -0,0 +1,9 @@
+
+#! Str functions 
+name = "Adarsh Bhardwaj"
+
+print(len(name))
+
+print(f"{name.endswith("j")} --> {name[len(name)-1]}")
+
+print(f'{name.isspace} :  true/false...')
---
+++ chapter_03/04_escape_seq.py
@@ -0,0 +1,12 @@
+
+#! Escape Seq 
+
+
+intro = "Adarsh Bhardwaj \nI'm a Flutter Developer\nand become an \"Ai Software Engineer\" \\\\"
+
+
+print(intro)
+
+
+
+#? 01:40:20
