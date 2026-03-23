f391ba79fd077c70fee4fe6f7da444b6c223d79f
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-24 00:43:15 +0530

    chapter_03_practice_set_02

---
+++ chapter_03/practice_set/01_problem1.py
@@ -0,0 +1,4 @@
+name  = input("Enter your name -> \t")
+
+
+print(f"Good night {name}")
---
+++ chapter_03/practice_set/02_problem2.py
@@ -0,0 +1,23 @@
+
+#! 02 Practice set____
+from datetime import datetime
+name = input("Enter your name --> ")
+
+now =  datetime.now()
+
+greeting = '''Dear <|name|>,
+You are selected as a Ai Software Engineer.
+You can join to <|date|>'''
+day = 0
+month = 0
+if(now.day+20> 30):
+    day = 20-(30-now.day)
+    month = now.month+1
+    print(f" ----> {day}")
+
+
+
+joining_date = f"{now.day}"
+
+
+print(greeting.replace("<|name|>",name).replace("<|date|>",f"{day}/{month}"))
