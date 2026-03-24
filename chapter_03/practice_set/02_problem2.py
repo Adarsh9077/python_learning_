e70689d35f395b55af9ae365ed9279bda1c00027
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-25 00:46:18 +0530

    complete practice set 02

--- chapter_03/practice_set/02_problem2.py
+++ chapter_03/practice_set/02_problem2.py
@@ -10,14 +10,19 @@ You are selected as a Ai Software Engineer.
 You can join to <|date|>'''
 day = 0
 month = 0
+year = 0
 if(now.day+20> 30):
     day = 20-(30-now.day)
     month = now.month+1
+    year = now.year
     print(f" ----> {day}")
+    if (month> 12 ):
+        month = 1
+        year = year +1
 
 
 
 joining_date = f"{now.day}"
 
 
-print(greeting.replace("<|name|>",name).replace("<|date|>",f"{day}/{month}"))
+print(greeting.replace("<|name|>",name).replace("<|date|>",f"{day}/{month}/{year}"))
