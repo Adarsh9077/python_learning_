03c3d72cf3afcbc9abc00ba6d6a5465dc1a44a70
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-26 00:51:41 +0530

    List usderstanding ....

--- chapter_04/01_list.py
+++ chapter_04/01_list.py
@@ -1,12 +1,25 @@
 
 #! List in python
+#? List are mutable (it can be change when requirements of user or programer)
+friends = ["Apple","Orange",5,3.14,False, "Adarsh", "John"]
+
+
+print(friends[0])
+
+
+numList = [12,1231,345345,32423,12345467,76,421,133]
+
+numList.sort()
+
+numList.reverse()
+
+numList.insert(1,69696969696996)
+
+numList.append(9999999)
+
+print(numList)
 
 
 
 
-
-
-
-
-
-#? 01:51:00
+#? 02:06:45
