dc306df2866a2dafa915ddfa3860e88f7cf506f1
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-03-27 00:38:47 +0530

    tupels and list

---
+++ chapter_04/02_tuple.py
@@ -0,0 +1,21 @@
+
+
+#! Tuples in python
+
+
+#? Tuples are immutable, it means you can not change the value after the initialization
+
+
+listOfStudent = ("john","doe","sara","smith","ford","ferrari")
+
+print(listOfStudent)
+
+#* methods in tuples:
+
+
+print(listOfStudent.index("john"))
+print(len(listOfStudent))
+
+j,d,s,sm,f,fr = listOfStudent
+
+print(j,d,s,sm,f,fr)
---
+++ chapter_04/prectice_set/01_problem1.py
@@ -0,0 +1,12 @@
+listOfFruitsName = []
+i=0
+while i<=6 :
+    fruitName = input("Enter a fruit name: -> ")
+    listOfFruitsName.append(fruitName)
+    i = i+1
+    
+print(len(listOfFruitsName))
+
+
+
+#! 02:18:00  
