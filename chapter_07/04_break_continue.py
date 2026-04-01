20b0c7471c2df288fc040b381cd5833f852fe5ac
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-02 01:37:00 +0530

    Working on break_continue_pass_statements

---
+++ chapter_07/04_break_continue.py
@@ -0,0 +1,30 @@
+
+#! Break and continue 
+
+
+for i in range (100):
+    if(i == 69):
+        print(f"i love \"{i}\"\nIf You know, you know")
+        break
+    print(i)
+    
+    
+print("\n============================\n")
+    
+for i in range(45, 112):
+    if(i == 69):
+        print(f"I too much love \"{i}\"\nOhhhh yeh..........")
+        continue
+        
+    print(i)
+    
+    
+    
+for i in range (10):
+    print(i)
+    # break
+    pass #! in pass statement compiler was running in backend...... ne careful about it
+
+
+
+print("pass function was their..............")
