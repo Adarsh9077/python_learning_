604bea11cbef647454aa6ef850d68d31ddd5b9a1
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-06 01:14:14 +0530

    Function,Functions with arguments

---
+++ chapter_08/01_functions.py
@@ -0,0 +1,21 @@
+
+#! Functions in python
+#? A function is a group of statement performing a specific task
+
+#? When a program gets bigger in size and its complexity grows, it gets difficult for a progran to keep track on which piece of code is doing whats! 
+
+
+#* this function definition
+def avg():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter first number: "))
+    c = int(input("Enter first number: "))
+    print(f"Your avg number is: {(a+b+c)/3}")
+    
+    
+#* this is function calls below
+avg()
+avg()
+avg()
+avg()
+avg()
---
+++ chapter_08/02_functions_with_arguments.py
@@ -0,0 +1,10 @@
+
+#! Functions with Arguments 
+
+def greeting (name,ending):
+    print("Hello, "+name+"\n"+ending)
+    
+    
+greeting("asdf","qwerty")
+
+#? 05:06:20
