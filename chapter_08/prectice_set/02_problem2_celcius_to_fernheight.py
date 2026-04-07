61cabe8d7af6a34776230efe155f37e9210cb103
Adarsh9077 <bhardwajadarsh873@gmail.com>
2026-04-08 01:05:15 +0530

    func prectise

---
+++ chapter_08/prectice_set/02_problem2_celcius_to_fernheight.py
@@ -0,0 +1,15 @@
+
+#! Celcius to ferheneihgt 
+
+
+def ferToCel(fern):
+    return 5*(fern-32)/9
+
+f = int(input("Enter temperature in F: "))
+
+c = ferToCel(f)
+
+print(f"The {f} to {round(c,2)} 0 C")
+
+
+#! 05:22:30
